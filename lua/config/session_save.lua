f25c329da38763c8701b565bea1c103f2fba201c
Raphael Molesim <raphael.molesim@gmail.com>
2025-03-02 12:17:20 -0300

    Fix bug in the session save

--- lua/config/session_save.lua
+++ lua/config/session_save.lua
@@ -1,15 +1,25 @@
 vim.o.sessionoptions = "buffers,curdir,folds,globals,help,tabpages,winsize"
 
-local session_file = vim.fn.stdpath("data") .. "/session.vim"
+-- Capture the initial directory when nvim starts.
+local initial_dir = vim.fn.getcwd()
+-- remove the first character if it is a slash
+if initial_dir:sub(1,1) == "/" then
+    initial_dir = initial_dir:sub(2)
+end
+
+-- Create a dedicated folder for sessions.
+local session_dir = vim.fn.stdpath("data") .. "/sessions"
+vim.fn.mkdir(session_dir, "p")
+
+-- Generate a session file name by replacing slashes with underscores.
+local session_file = session_dir .. "/" .. vim.fn.substitute(initial_dir, "[/]", "_", "g") .. ".vim"
+
 vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function()
+    callback = function()	
         vim.cmd("mksession! " .. session_file)
     end,
 })
 
-
-local session_file = vim.fn.stdpath("data") .. "/session.vim"
 if vim.fn.filereadable(session_file) == 1 then
     vim.cmd("source " .. session_file)
 end
-
